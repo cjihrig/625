d051f7d250e5168c751f3c9f5b1ec71c87a57dab
cjihrig <cjihrig@gmail.com>
2015-11-22 20:59:44 -0500

    add hack for setting es2015 preset

--- bin/625.js
+++ bin/625.js
@@ -11,6 +11,14 @@ var options = {
   stdio: 'inherit',
   encoding: 'utf8'
 };
+
+// FIXME: Ideally, this should load our local .babelrc. However, specifying
+// the .babelrc path from the command line does not seem to be supported.
+// Since we spawn Babel with the caller's working directory, Babel won't
+// see our .babelrc. The workaround is to append our settings to the existing
+// arguments. Currently, that just means using the es2015 preset.
+args.push('--presets', require.resolve('babel-preset-es2015'));
+
 var babel = ChildProcess.spawn(cmd, args, options);
 
 babel.on('error', console.error);
